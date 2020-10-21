35e858dd10c5a6c3c93f59b98a5a277a1ecd6929
shahebaz66 <60848272+shahebaz66@users.noreply.github.com>
2020-10-21 17:56:19 +0530

    Add files via upload

---
+++ storeModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+mongoose.set('debug', true);
+mongoose.Promise = Promise;
+//mongodb://127.0.0.1:27017/cloud?retryWrites=true&w=majority
+// mongoose
+//   .connect(``, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//     useUnifiedTopology: true
+//   })
+//   .then(() => console.log('DB connection successfull!'));
+const visitSchema = new Schema({
+    type: String,
+    url: String,
+    category: String
+});
+
+module.exports = Store = mongoose.model('Store', visitSchema);
